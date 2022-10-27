969cc62dfb220ae0e634a0bd55dcf00203c1d181
Didier van Os <ngon-@hotmail.com>
2022-10-27 13:07:40 +0100

    Add error handling

--- Express/BasicInformational/app.js
+++ Express/BasicInformational/app.js
@@ -5,10 +5,19 @@ const PORT = 8080;
 
 const app = express()
 app.use(express.static('public', {extensions: ['html']}))
-app.use('/', (req, res) => {
-  res
-    .status(404)
-    .sendFile(path.resolve('public', '404.html'))
+app.use('/', (req, res, next) => {
+  try {
+    res
+      .status(404)
+      .sendFile(path.resolve('public', '404.html'))  
+  } catch (err) {
+    next(err)
+  }
+  
+})
+app.use('/', (err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Error 500: Internal Server Error')
 })
 app.listen(PORT)
 
