41a9203fb45a1d781829c8065b9a94aa8e259e64
Didier van Os <ngon-@hotmail.com>
2022-10-30 12:18:22 +0000

    MDN Express part 4

--- Express/express-locallibrary-tutorial/app.js
+++ Express/express-locallibrary-tutorial/app.js
@@ -6,10 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
 var app = express();
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://local_library:cKBquTlDqpcZ4rZZ@cluster0.bwgo4qm.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = ;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
